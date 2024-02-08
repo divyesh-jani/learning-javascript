e28a5a3f24ba7f1603047243a52b14de80115d1d
Divyesh Jani <djani03@gmail.com>
2024-02-07 20:37:30 -0500

    Adding search algorithms

--- Searching/binary_search.js
+++ Searching/binary_search.js
@@ -60,3 +60,25 @@ console.log('Array length: ' + arrayToSearchTwo.length);      // Array length: 1
 let resultArrayOddTwo = searchForElementsTwo.map((a) => binarySearchTwo(arrayToSearchTwo, a));
 console.log(resultArrayOddTwo);
 // [ f, t, f, t, f, t, f, t, f, t, f, t, f, f, t, f, t, f, t, f, t, f ,f ]
+
+function getIndexUsingBinarySearch(array, value) {
+    if (array.length === 0) return -1;
+    let start = 0;
+    let end = array.length - 1;
+    while (start <= end) {
+        let middle = Math.floor((start + end)/2);
+        if (array[middle] === value) {
+            return middle;
+        } else if (array[middle] < value) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+    return -1;
+}
+
+let arrayToSearchThree = [0, 4, 10, 12, 15, 15, 24, 30, 30, 30, 40, 50];
+let searchForElementsThree = [0, 10, 15, 24, 30, 40, 50, -2, 20, 45, 55]; // [0, 2, 5, 6, 8, 10, 11, -1, -1, -1, -1]
+let resultArrayOfIndexes = searchForElementsThree.map((a) => getIndexUsingBinarySearch(arrayToSearchThree, a));
+console.log(resultArrayOfIndexes);
