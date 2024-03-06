e317ea262d9b1bd4374fb78a41aaff3960605f0f
Divyesh Jani <djani03@gmail.com>
2024-03-06 15:13:59 -0500

    Extending child class after inheritance

--- Basics/class_inheritance.js
+++ Basics/class_inheritance.js
@@ -17,27 +17,35 @@ class Person {
 }
 
 class SuperHero extends Person {
-    constructor(first, last) {
+    constructor(first, last, world) {
         super(first, last);
+        this.virtualWorld = world || 'Reality';
         this.isSuperHero = true;
     }
 
     getFullName() {
         console.log('I am a SuperHero and my name is ' + this.firstName + ' ' + this.lastName);
     }
+
+    get myWorld() {
+        return 'My world is ' + this.virtualWorld + '!';
+    }
 }
-
-
 
 let s1 = new SuperHero('Bruce', 'Wayne');
 console.log(s1);                // type: SuperHero
 s1.getFullName();               // I am a SuperHero and my name is Bruce Wayne
 console.log(s1.isActive);       // true
-s1.getStatus()                  // I am available! Need help?
+s1.getStatus();                 // I am available! Need help?
+console.log(s1.myWorld);        // My world is Reality!
 
 
-let s2 = new SuperHero('Diana', 'Prince');
-console.log(s2);                // type: SuperHero
+let s2 = new SuperHero('Diana', 'Prince', 'DC');
 s2.getFullName();               // I am a SuperHero and my name is Diana Prince
 console.log(s2.isActive);       // true
 s2.getStatus();                 // I am available! Need help?
+console.log(s2.myWorld);        // My world is DC!
+
+let s3 = new SuperHero('Bruce', 'Banner', 'Marvel');
+s3.getFullName();               // I am a SuperHero and my name is Bruce Banner
+console.log(s3.myWorld);        // My world is Marvel!
