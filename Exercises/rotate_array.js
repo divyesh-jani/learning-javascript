c2bbcb8a6720dd14d2a31c5ca648d71ea0a55aa0
Divyesh Jani <djani03@gmail.com>
2024-03-08 15:03:57 -0500

    Handle /0 case

--- Exercises/rotate_array.js
+++ Exercises/rotate_array.js
@@ -2,6 +2,7 @@
 
 // Straightforward way, not the most efficient though
 function rotateArray(array, k) {
+    if (k === 0 || k % array.length === 0) return array;
     let shifts = k % array.length;
     while (shifts > 0) {
         array.unshift(array.pop());
@@ -11,6 +12,8 @@ function rotateArray(array, k) {
 }
 
 let arrayToRotate = [1,2,3,5,8,13,21];
+console.log(rotateArray(arrayToRotate, 0));     // [1,2,3,5,8,13,21]
+arrayToRotate = [1,2,3,5,8,13,21];
 console.log(rotateArray(arrayToRotate, 1));     // [21,1,2,3,5,8,13]
 arrayToRotate = [1,2,3,5,8,13,21];
 console.log(rotateArray(arrayToRotate, 2));     // [13,21,1,2,3,5,8]
@@ -24,24 +27,26 @@ console.log(rotateArray(arrayToRotate, 7));     // [1,2,3,5,8,13,21]
 
 // Alternate approach which swaps values instead of pushing and popping
 function rotateArrayBySwapping(array, k) {
+    if (k === 0 || k % array.length === 0) return array;
     let shifts = k % array.length;
     let lastIndex = array.length - 1;
-    reversePartOfArray(array, 0, lastIndex);           // Reverse entire array
-    reversePartOfArray(array, 0, shifts - 1);          // Reverse everything before shifts (k)
-    reversePartOfArray(array, shifts, lastIndex);      // Reverse from shifts (k) to end
-    return array;
-}
-
-function reversePartOfArray(array, start, end) {
-    while (start < end) {
-        [array[start], array[end]] = [array[end], array[start]];
-        start = start + 1;
-        end = end - 1;
+    function reversePartOfArray(start, end) {
+        while (start < end) {
+            [array[start], array[end]] = [array[end], array[start]];
+            start = start + 1;
+            end = end - 1;
+        }
     }
+    reversePartOfArray(0, lastIndex);           // Reverse entire array
+    reversePartOfArray(0, shifts - 1);          // Reverse everything before shifts (k)
+    reversePartOfArray(shifts, lastIndex);      // Reverse from shifts (k) to end
+    return array;
 }
 
 // Testing
 let testArray = [1,2,3,5,8,13,21];
+console.log(rotateArrayBySwapping(testArray, 0));     // [1,2,3,5,8,13,21]
+testArray = [1,2,3,5,8,13,21];
 console.log(rotateArrayBySwapping(testArray, 1));     // [21,1,2,3,5,8,13]
 testArray = [1,2,3,5,8,13,21];
 console.log(rotateArrayBySwapping(testArray, 2));     // [13,21,1,2,3,5,8]
