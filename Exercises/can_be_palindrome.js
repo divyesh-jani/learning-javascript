4b1e3d0e98d5a2e5894205c0a878be9c1754668b
Divyesh Jani <djani03@gmail.com>
2024-04-03 10:27:08 -0400

    Refactoring exercises: can be palindrome and n missing numbers

--- Exercises/can_be_palindrome.js
+++ Exercises/can_be_palindrome.js
@@ -4,33 +4,28 @@ function canBePalindrome(str) {
     let start = 0;
     let end = str.length - 1;
     let changed = false;
-    while (start <= end) {
-        if (str[start] === str[end]) {
-            start = start + 1;
-            end = end - 1;
-        } else {
-            if (changed) {
-                return false;
-            }
+    while (start < end) {
+        if (str[start] !== str[end]) {
+            if (changed) return false;
+            changed = true;
             if (str[start] === str[end - 1]) {
                 end = end - 1;
-            } else if (str[end] === str[start + 1]) {
+            } else if (str[start + 1] === str[end]) {
                 start = start + 1;
             } else {
                 return false;
             }
-            start = start + 1;
-            end = end - 1;
-            changed = true;
         }
+        start = start + 1;
+        end = end - 1;
     }
     return true;
 }
 
-console.log(canBePalindrome('ksayak'));  // true
-console.log(canBePalindrome('racecars')); // true
-console.log(canBePalindrome('adam')); // true
-console.log(canBePalindrome('kpayakp')); // false
-console.log(canBePalindrome('toot')); // true
-console.log(canBePalindrome('toeot')); // true
-console.log(canBePalindrome('tootem')); // false
+console.log(canBePalindrome('ksayak'));     // true
+console.log(canBePalindrome('racecars'));   // true
+console.log(canBePalindrome('adam'));       // true
+console.log(canBePalindrome('kpayakp'));    // false
+console.log(canBePalindrome('toot'));       // true
+console.log(canBePalindrome('toeot'));      // true
+console.log(canBePalindrome('tootem'));     // false
--- Exercises/n_missing_numbers.js
+++ Exercises/n_missing_numbers.js
@@ -2,16 +2,21 @@
 // Eg. input = [2,4,6,7], n = 2 => [3, 5]
 
 function getNMissingNumbers(nums, n) {
+    let expected = nums[0] + 1;
     let missingNums = [];
-    let lastNum = nums[0];
-    let i = 1;
-    while (missingNums.length < n && i < nums.length) {
-        while (missingNums.length < n && nums[i] !== lastNum + 1) {
-            missingNums.push(lastNum + 1);
-            lastNum = lastNum + 1;
+    for (let i = 1; i < nums.length; i = i + 1) {
+        while (nums[i] !== expected) {
+            missingNums.push(expected);
+            expected = expected + 1;
+            if (missingNums.length === n) {
+                return missingNums;
+            }
         }
-        lastNum = nums[i];
-        i = i + 1;
+        expected = expected + 1;
+    }
+    while (missingNums.length < n) {
+        missingNums.push(expected);
+        expected = expected + 1;
     }
     return missingNums;
 }
@@ -19,3 +24,4 @@ function getNMissingNumbers(nums, n) {
 console.log(getNMissingNumbers([2,4,6,7], 2));              // [3, 5]
 console.log(getNMissingNumbers([100,104,105,106,110], 5));  // [101, 102, 103, 107, 108]
 console.log(getNMissingNumbers([46,47,48,50,500], 4));      // [49, 51, 52, 53]
+console.log(getNMissingNumbers([4,6,8], 5));                // [ 5, 7, 9, 10, 11 ]
