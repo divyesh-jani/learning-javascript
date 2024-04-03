9fd3b3e9cc39c4f1e59ab90551bcaafe04b80f12
Divyesh Jani <djani03@gmail.com>
2024-04-02 21:26:31 -0400

    Updated binary gap exercise

--- Exercises/binary_gap.js
+++ Exercises/binary_gap.js
@@ -6,23 +6,14 @@
  * Eg. input = 11; result = 1 ; (11 = 1011)
  */
 
-function convertToBinary(N) {
-    let binaryArr = [];
-    while (N > 1) {
-        let num = N % 2;
-        binaryArr.unshift(num);
-        N = Math.floor(N / 2);
-    }
-    binaryArr.unshift(1);
-    return findZeros(binaryArr);
-}
-
-function findZeros(arr) {
+function findZeros(N) {
+    if (N < 5) return 0;
+    let binaryArr = convertToBinary(N);
     let maxZeros = 0;
     let i = 1;
     let currZeros = 0;
-    while (i < arr.length) {
-        if (arr[i] === 0) {
+    while (i < binaryArr.length) {
+        if (binaryArr[i] === 0) {
             currZeros = currZeros + 1;
         } else {
             if (currZeros > maxZeros) maxZeros = currZeros;
@@ -33,10 +24,24 @@ function findZeros(arr) {
     return maxZeros;
 }
 
-console.log(convertToBinary(9));        // 2 (1001)
-console.log(convertToBinary(15));       // 0 (1111)
-console.log(convertToBinary(20));       // 1 (10100)
-console.log(convertToBinary(32));       // 0 (100000)
-console.log(convertToBinary(165));      // 2 (10100101)
-console.log(convertToBinary(529));      // 4 (1000010001)
-console.log(convertToBinary(1041));     // 5 (10000010001)
+function convertToBinary(N) {
+    let binaryArr = [];
+    while (N > 1) {
+        let num = N % 2;
+        binaryArr.unshift(num);
+        N = Math.floor(N / 2);
+    }
+    binaryArr.unshift(1);
+    return binaryArr;
+}
+
+console.log(findZeros(0));          // 0 (0)
+console.log(findZeros(2));          // 0 (10)
+console.log(findZeros(5));          // 1 (101)
+console.log(findZeros(9));          // 2 (1001)
+console.log(findZeros(15));         // 0 (1111)
+console.log(findZeros(20));         // 1 (10100)
+console.log(findZeros(32));         // 0 (100000)
+console.log(findZeros(165));        // 2 (10100101)
+console.log(findZeros(529));        // 4 (1000010001)
+console.log(findZeros(1041));       // 5 (10000010001)
