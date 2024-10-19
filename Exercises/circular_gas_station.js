812f8292bd4a3570fb9292c1cf37dc05afcff5d1
Divyesh Jani <djani03@gmail.com>
2024-10-19 13:45:10 -0400

    chore: correct function name

--- Exercises/circular_gas_station.js
+++ Exercises/circular_gas_station.js
@@ -1,6 +1,6 @@
 // Circular gas station, figure out where to start to end up at same gas station without running out of gas
 
-function gasStationPathUsingFor(gasArray, costArray) {
+function gasStationPath(gasArray, costArray) {
     let availableGas = 0;
     let previousAvailableGas = 0;
     let startStation = 0;
@@ -20,9 +20,9 @@ function gasStationPathUsingFor(gasArray, costArray) {
 
 let gasStations = [1,2,3,4,5];
 let costAtStations = [3,4,5,1,2];
-console.log(gasStationPathUsingFor(gasStations, costAtStations));   // 3
+console.log(gasStationPath(gasStations, costAtStations));   // 3
 
 
 let availableGas = [1, 5, 3, 3, 5, 3, 1, 3, 4, 5];
 let costToGoToNext = [5, 2, 2, 8, 2, 4, 2, 5, 1, 2];
-console.log(gasStationPathUsingFor(availableGas, costToGoToNext));  // 8
+console.log(gasStationPath(availableGas, costToGoToNext));  // 8
